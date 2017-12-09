966b9789c9bd205df9fb5088dbc73aa924a686cd
kstawinski <m@stawinski.me>
2017-12-09 21:45:10 +0100

    Update

--- assets/quiz.js
+++ assets/quiz.js
@@ -11,17 +11,17 @@ const pointsGeneral = quiz.querySelector("#points_general");
 
 const questions = [];
 // questions[index] = ["question", "number of correct answer", "answer 1", "answer 2", "answer 3", "answer 4"];
-questions[0] = ["What is the HTML element to bold a text?", "1", "&lt;b&gt;", "&lt;bold&gt;", "&lt;wide&gt;", "&lt;big&gt;"];
-questions[1] = ["Who is making the Web standards?", "3", "Microsoft", "Google", "The World Wide Web Consortium", "Mozilla"];
-questions[2] = ["What does HTML stand for?", "1", "Hyper Text Markup Language", "Hyperlinks and Text Markup Language", "Home Tool Markup Language", "Highly Text Markup Language"];
-questions[3] = ["What is the HTML tag for a link?", "3", "&lt;link&gt;", "&lt;ref&gt;", "&lt;a&gt;", "&lt;hper&gt;"];
-questions[4] = ["Which is the correct CSS syntax?", "3", "body:color=black;", "{body;color:black;}", "body {color: black;}", "{body:color=black;}"];
-questions[5] = ["How do you write 'Hello World' in PHP?", "2", "'Hello World'", "echo 'Hello World';", "document.write('Hello World');", "echo('Hello World');"];
-questions[6] = ["What does CSS stand for?", "4", "Creative Style Sheets", "Computer Style Sheets", "Colorful Style Sheets", "Cascading Style Sheets"];
-questions[7] = ["Where in an HTML document is the correct place to refer to an external style sheet?", "4", "In the &lt;body&gt; section", "At the end of the document", "At the top of the document", "In the &lt;head&gt; section"];
-questions[8] = ["What does PHP stand for?", "2", "Personal Home Page", "PHP: Hypertext Preprocessor", "Personal Hypertext Processor", "Private Home Page"];
-questions[9] = ["Which property is used to change the background color?", "3", "bgcolor", "bg-color", "background-color", "color"];
-questions[10] = ["Which HTML tag is used to define an internal style sheet?", "1", "&lt;style&gt;", "&lt;script&gt;", "&lt;css&gt;", "&lt;link&gt;"];
+questions[0] = ["What is the HTML element to bold a text?", 1, "&lt;b&gt;", "&lt;bold&gt;", "&lt;wide&gt;", "&lt;big&gt;"];
+questions[1] = ["Who is making the Web standards?", 3, "Microsoft", "Google", "The World Wide Web Consortium", "Mozilla"];
+questions[2] = ["What does HTML stand for?", 1, "Hyper Text Markup Language", "Hyperlinks and Text Markup Language", "Home Tool Markup Language", "Highly Text Markup Language"];
+questions[3] = ["What is the HTML tag for a link?", 3, "&lt;link&gt;", "&lt;ref&gt;", "&lt;a&gt;", "&lt;hper&gt;"];
+questions[4] = ["Which is the correct CSS syntax?", 3, "body:color=black;", "{body;color:black;}", "body {color: black;}", "{body:color=black;}"];
+questions[5] = ["How do you write 'Hello World' in PHP?", 2, "'Hello World'", "echo 'Hello World';", "document.write('Hello World');", "echo('Hello World');"];
+questions[6] = ["What does CSS stand for?", 4, "Creative Style Sheets", "Computer Style Sheets", "Colorful Style Sheets", "Cascading Style Sheets"];
+questions[7] = ["Where in an HTML document is the correct place to refer to an external style sheet?", 4, "In the &lt;body&gt; section", "At the end of the document", "At the top of the document", "In the &lt;head&gt; section"];
+questions[8] = ["What does PHP stand for?", 2, "Personal Home Page", "PHP: Hypertext Preprocessor", "Personal Hypertext Processor", "Private Home Page"];
+questions[9] = ["Which property is used to change the background color?", 3, "bgcolor", "bg-color", "background-color", "color"];
+questions[10] = ["Which HTML tag is used to define an internal style sheet?", 1, "&lt;style&gt;", "&lt;script&gt;", "&lt;css&gt;", "&lt;link&gt;"];
 
 let pcorrect = 0;
 let pgeneral = 0; 
@@ -40,19 +40,16 @@ function newQuestion(number) {
   answers.onclick = function(event) {
 	const target = getEventTarget(event);
 	
-	if(target.value === questions[number][1]) {
-	  target.classList.add("correct");
-	  
+	if(parseInt(target.value) === questions[number][1]) {
 	  pcorrect++;
 	  pointsCorrect.innerHTML = pcorrect;
-	}
-	else {
-	  target.classList.add("incorrect");
 	}
 	
   // add 1 point to general points
   pgeneral++;
   pointsGeneral.innerHTML = pgeneral;
+
+  newQuestion(rand(0, questions.length-1));
 	
   }
 }
@@ -62,36 +59,6 @@ newQuestion(rand(0, questions.length-1));
 nextQuestion.addEventListener("click", function(){
   newQuestion(rand(0, questions.length-1));
 });
-
-nextQuestion.addEventListener("click", clearClasses);
-
-// clearing unnecessary classes
-function clearClasses() {
-  if(answer1.classList.contains("bad")) {
-	answer1.classList.remove("bad");
-  }
-  if(answer2.classList.contains("bad")) {
-	answer2.classList.remove("bad");
-  }
-  if(answer3.classList.contains("bad")) {
-	answer3.classList.remove("bad");
-  }
-  if(answer4.classList.contains("bad")) {
-	answer4.classList.remove("bad");
-  }
-  if(answer1.classList.contains("correct")) {
-	answer1.classList.remove("correct");
-  }
-  if(answer2.classList.contains("correct")) {
-	answer2.classList.remove("correct");
-  }
-  if(answer3.classList.contains("correct")) {
-	answer3.classList.remove("correct");
-  }
-  if(answer4.classList.contains("correct")) {
-	answer4.classList.remove("correct");
-  }
-}
 
 function getEventTarget(e) {
   e = e || window.event;
