b9b4b59d868a6d94a54aaa0e8c7437ac8edbc66a
kstawinski <m@stawinski.me>
2018-03-11 00:31:00 +0100

    Questions corrected

--- assets/quiz.js
+++ assets/quiz.js
@@ -13,16 +13,15 @@ const result = quiz.querySelector(".result");
 const questions = [];
 // questions[index] = ["question", "number of correct answer", "answer 1", "answer 2", "answer 3", "answer 4"];
 questions[0] = ["What is the HTML element to bold a text?", 1, "&lt;b&gt;", "&lt;bold&gt;", "&lt;wide&gt;", "&lt;big&gt;"];
-questions[1] = ["Who is making the Web standards?", 3, "Microsoft", "Google", "The World Wide Web Consortium", "Mozilla"];
+questions[1] = ["Which HTML tag is used to define an internal style sheet?", 1, "&lt;style&gt;", "&lt;script&gt;", "&lt;css&gt;", "&lt;link&gt;"];
 questions[2] = ["What does HTML stand for?", 1, "Hyper Text Markup Language", "Hyperlinks and Text Markup Language", "Home Tool Markup Language", "Highly Text Markup Language"];
 questions[3] = ["What is the HTML tag for a link?", 3, "&lt;link&gt;", "&lt;ref&gt;", "&lt;a&gt;", "&lt;hper&gt;"];
 questions[4] = ["Which is the correct CSS syntax?", 3, "body:color=black;", "{body;color:black;}", "body {color: black;}", "{body:color=black;}"];
-questions[5] = ["How do you write 'Hello World' in PHP?", 2, "'Hello World'", "echo 'Hello World';", "document.write('Hello World');", "echo('Hello World');"];
+questions[5] = ["How do you write 'Hello World' in PHP?", 2, "'Hello World'", "echo 'Hello World';", "document.write('Hello World');", "write Hello World"];
 questions[6] = ["What does CSS stand for?", 4, "Creative Style Sheets", "Computer Style Sheets", "Colorful Style Sheets", "Cascading Style Sheets"];
 questions[7] = ["Where in an HTML document is the correct place to refer to an external style sheet?", 4, "In the &lt;body&gt; section", "At the end of the document", "At the top of the document", "In the &lt;head&gt; section"];
 questions[8] = ["What does PHP stand for?", 2, "Personal Home Page", "PHP: Hypertext Preprocessor", "Personal Hypertext Processor", "Private Home Page"];
 questions[9] = ["Which property is used to change the background color?", 3, "bgcolor", "bg-color", "background-color", "color"];
-questions[10] = ["Which HTML tag is used to define an internal style sheet?", 1, "&lt;style&gt;", "&lt;script&gt;", "&lt;css&gt;", "&lt;link&gt;"];
 
 let pcorrect = 0;
 let pgeneral = 0; 
